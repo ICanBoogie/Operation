79ac6a6a46032428d6dd32645d48a9c502bbbdc8
Olivier Laviale <olivier.laviale@gmail.com>
2015-09-07 23:17:39 +0200

    Using Errors::add()

--- tests/cases/response/has-errors.php
+++ tests/cases/response/has-errors.php
@@ -6,8 +6,8 @@ $response = new Response;
 
 $errors = $response->errors;
 
-$errors[] = $errors->format('A global error');
-$errors['title'] = $errors->format('An error on the %field field.', [ 'field' => 'Title']);
+$errors->add(null, 'A global error');
+$errors->add('title', "An error on the %field field.", [ 'field' => 'Title']);
 $errors['password'] = true; // an error on 'password', without message
 $errors['message'] = "Message too long";
 $errors['message'] = "Message too short";
