d4e34572ad49460e1560e2a0e7d91e85a22cd96c
Olivier Laviale <olivier.laviale@gmail.com>
2015-10-27 19:57:52 +0100

    Added OperationRouteDefinition

---
+++ lib/OperationRouteDefinition.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the ICanBoogie package.
+ *
+ * (c) Olivier Laviale <olivier.laviale@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ICanBoogie\Operation;
+
+use ICanBoogie\Routing\RouteDefinition;
+
+class OperationRouteDefinition extends RouteDefinition
+{
+	const PARAM_TRANSLATION_LIST = 'param_translation_list';
+}
